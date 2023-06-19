cd1ec67812d4526ea8fc4ad4a0eac3afa06f10a3
Danmainah <danielmainawaweru@gmail.com>
2023-06-19 18:10:26 +0300

    seting up the app and nav and home component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Nav } from './components/nav'
+import { Home } from './pages/home'
+
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Home />
+      <p> this is the app</p>
+    </>
+  )
+}
+
+export default App
